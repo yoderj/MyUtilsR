abc1a7704be91416a04ef6d8e0ef5ece85f3a030
MABadge <mabadgeley@gmail.com>
2017-12-28 18:41:17 -0800

    add str case methods for snake and camel

--- R/str.R
+++ R/str.R
@@ -1,3 +1,48 @@
+# Helpers ----
+
+if (interactive()) {
+    test_chrs <<- MyUtils::gen_test_suite()
+}
+
+#' Split a string vector into a list of string piece vectors.
+#'
+#' First, a replacement is performed at alphanumeric piece edges to create
+#' pieces that are delimited by a space or punctuation character.
+#' Second, the string is split by split_pat into pieces
+#'
+#' @param x_chr character vector of strings to be split
+#' @param split_pat regex pattern to delimit pieces
+#'
+#' @return pieces list with same length as x_chr, whose i-th element contains a
+#' character vector of split products of x_chr[i]
+#'
+#' @import rebus purrr stringr assertive.strings
+#' @export
+split_pieces <- function(x_chr, split_pat = character()) {
+    if (is_empty(split_pat)) {
+        split_pat <- one_or_more(rebus::or("[[:space:]]", "[[:punct:]]"))
+    }
+    nms <- names(x_chr) %||% x_chr
+
+    pieces <- x_chr %>%
+        str_replace_all(pattern=capture(LOWER) %R% capture(UPPER),
+                        replacement = "\\1 \\2") %>%
+        str_replace_all(pattern = capture(ALPHA) %R% capture(DIGIT),
+                        replacement = "\\1 \\2") %>%
+        str_replace_all(pattern = capture(DIGIT) %R% capture(ALPHA),
+                        replacement = "\\1 \\2") %>%
+        str_split(pattern=split_pat)
+
+    # Remove empty strings pieces
+    pieces %<>% lapply(FUN = discard, .p=is_empty_character)
+
+    names(pieces) <- nms
+    pieces
+}
+
+
+# Case Transformations ----
+
 #' Convert string to title case.
 #'
 #' Transform a character vector into a title case representation.
@@ -31,23 +76,35 @@ str_case_title <- function(x_chr) {
 
 #' Convert string to snake_case.
 #'
+#' Transform a character vector into a snake case representation.
+#' First, separate camelCase and alphanumeric boundaries into spaces
+#' Second, split pieces by symbols or spaces
+#' Third, convert all alpha to lower with \code{\link[stringr]{str_to_lower}}
+#' Finally, collapse with "_"
+#'
 #' @param x_chr character vector
 #'
-#' @return A character vector the same len as x_chr, with CamelCaseFormatting
+#' @return x_snake character vector with same length as x_chr, but snake format
+#'
+#' @import stringr
 #' @export
-# str_case_snake <- function(x_chr) {
-#     x_chr %>%
-#         stringr::str_replace_all(
-#             pattern = "_",
-#             replacement= " ") %>%
-#         stringr::str_to_title() %>%
-#         stringr::str_replace_all(
-#             pattern = " ",
-#             replacement= "")
-# }
+#'
+#' @family str_case
+str_case_snake <- function(x_chr) {
+    pieces <- split_pieces(x_chr)
+
+    pieces %>%
+        map(stringr::str_to_lower) %>%
+        map_chr(str_c, collapse="_")
+}
 
 
-#' Convert snake case to camel case by removing "_" and making initial letters cap
+#' Convert string to camel case.
+#'
+#' Transform a character vector into a camel case representation.
+#' First, convert `x_chr` to title case with \code{\link{str_case_title}}
+#' Second, remove spaces
+#' Third, lower first character of each str if `lower`
 #'
 #' @param x_chr A character vector with snake_case_formatting
 #'
@@ -56,22 +113,25 @@ str_case_title <- function(x_chr) {
 #' @importFrom magrittr "%>%"
 #' @import rebus
 #' @export
-#' @example
+#' @examples
 #' str_case_camel(x_chr = c("from_snake_case", "From Title Case"))
-# str_case_camel <- function(x_chr, upper=FALSE) {
-#     SPACE_PAT <- capture()
-#     DELIMITER_IN <- rebus::or("[:space:]", "[:punct:]")
-#
-#     x_spaced <-
-#     x_pieces <- x_chr %>%
-#
-#     x_chr %>%
-#         stringr::str_replace_all(
-#             pattern = "_",
-#             replacement= " ") %>%
-#         stringr::str_to_title() %>%
-#         stringr::str_replace_all(
-#             pattern = " ",
-#             replacement= "")
-# }
-#
+#' str_case_camel(x_chr = c("from_snake_case", "From Title Case"), lower=FALSE)
+str_case_camel <- function(x_chr, lower=TRUE) {
+    nms <- names(x_chr)
+
+    x_UpperCamelCase <- x_chr %>%
+        str_case_title() %>%
+        str_replace_all(pattern = " ", replacement = "")
+    names(x_UpperCamelCase) <- nms
+    if (lower) {
+        return(
+            (function(.x)
+            {
+                str_c(substr(.x, 0, 1) %>% tolower(),
+                      substr(.x, 2, nchar(.x)))
+            })(x_UpperCamelCase)
+        )
+    } else {
+        return(x_UpperCamelCase)
+    }
+}
--- inst/tests/testthat/test-str.R
+++ inst/tests/testthat/test-str.R
@@ -5,7 +5,7 @@ context("String methods")
 
 
 # Test Blocks ----
-test_that("test string case 2 title transformations", {
+test_that("test string case 2 Title Case transformations", {
     expect_title <- function(x_str, expect_str) {
         eval(bquote(
             expect_equivalent(object = str_case_title(.(x_str)), .(expect_str))
@@ -23,20 +23,26 @@ test_that("test string case 2 title transformations", {
     expect_title(" aA.", "A A")
 })
 
-test_that("test string case 2 title transformations", {
-    expect_title <- function(x_str, expect_str) {
+test_that("test string case 2 snake_case transformations", {
+    expect_snake <- function(x_str, expect_str) {
         eval(bquote(
-            expect_equivalent(object = str_case_title(.(x_str)), .(expect_str))
+            expect_equivalent(object = str_case_snake(.(x_str)), .(expect_str))
         ))
     }
 
-    #! Issue #53
-    # test suite of vectorized objects against cached reference
-    # expect_equal_to_reference(object = map(test_chrs, str_case_title),
-    #                           file = system.file("tests", "testthat", package = "MyUtils") %>%
-    #                               file.path("test-str-testcache.rds"))
+    expect_snake("Aa_(w", "aa_w")
+    expect_snake(" aA.", "a_a")
+})
 
-    # test edge case scalar strings
-    expect_title("Aa_(w", "Aa W")
-    expect_title(" aA.", "A A")
+test_that("test string case 2 camelCase transformations", {
+    expect_camel <- function(x_str, expect_str, lower=TRUE) {
+        eval(bquote(
+            expect_equivalent(object=str_case_camel(.(x_str), .(lower)), .(expect_str))
+        ))
+    }
+
+    expect_camel("Aa_(w", "aaW")
+    expect_camel(" aA.", "aA")
+    expect_camel("Aa_(w", "AaW", lower=FALSE)
+    expect_camel(" aA.", "AA", lower=FALSE)
 })
